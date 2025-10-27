6d65451910b5491a5b1a151749c41722a7553e92
andrewmyton <159046575+andrewmyton@users.noreply.github.com>
2025-10-27 13:47:02 +0000

    chore: post can now display image

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,11 +1,15 @@
 import PropTypes from 'prop-types'
 import { User } from './User.jsx'
 
-export function Post({ title, contents, author }) {
+export function Post({ title, contents, imageURL, author }) {
   return (
     <article>
       <h3>{title}</h3>
       <div>{contents}</div>
+      <div>
+        <img src={imageURL} alt={title} height='200' width='200' />
+      </div>
+
       {author && (
         <em>
           <br />
@@ -20,4 +24,5 @@ Post.propTypes = {
   title: PropTypes.string.isRequired,
   contents: PropTypes.string,
   author: PropTypes.string,
+  imageURL: PropTypes.string,
 }
