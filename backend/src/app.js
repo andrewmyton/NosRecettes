11d2966129094452e740244b9151ccf09a4e2b93
andrewmyton <159046575+andrewmyton@users.noreply.github.com>
2025-11-04 02:33:28 +0000

    chore: end ch11

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,9 +2,29 @@ import express from 'express'
 import cors from 'cors'
 import bodyParser from 'body-parser'
 import { eventRoutes } from './routes/events.js'
-
 import { postsRoutes } from './routes/posts.js'
 import { userRoutes } from './routes/users.js'
+import { ApolloServer } from '@apollo/server'
+import { expressMiddleware } from '@apollo/server/express4'
+import { typeDefs, resolvers } from './graphql/index.js'
+import { optionalAuth } from './middleware/jwt.js'
+
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+apolloServer.start().then(() =>
+  app.use(
+    '/graphql',
+    optionalAuth,
+    expressMiddleware(apolloServer, {
+      context: async ({ req }) => {
+        return { auth: req.auth }
+      },
+    }),
+  ),
+)
 
 const app = express()
 app.use(cors())
