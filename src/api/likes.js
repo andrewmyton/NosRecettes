13fc15a3eb61846259797ddfeaac08d33582b7d6
andrewmyton <159046575+andrewmyton@users.noreply.github.com>
2025-11-17 16:24:42 +0000

    chore: added likes js file

---
+++ src/api/likes.js
@@ -0,0 +1,11 @@
+export const updateLikes = async (token, post) => {
+  const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/posts`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(post),
+  })
+  return await res.json()
+}
