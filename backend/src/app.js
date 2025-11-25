9ecf1d3cd53ba280263bfbf7a18183eabdc4f77e
andrewmyton <159046575+andrewmyton@users.noreply.github.com>
2025-11-25 13:38:22 +0000

    chore: added socket server

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,30 +1,10 @@
 import express from 'express'
 import cors from 'cors'
 import bodyParser from 'body-parser'
-import { eventRoutes } from './routes/events.js'
+import { createServer } from 'node:http'
+import { Server } from 'socket.io'
 import { postsRoutes } from './routes/posts.js'
 import { userRoutes } from './routes/users.js'
-import { ApolloServer } from '@apollo/server'
-import { expressMiddleware } from '@apollo/server/express4'
-import { typeDefs, resolvers } from './graphql/index.js'
-import { optionalAuth } from './middleware/jwt.js'
-
-const apolloServer = new ApolloServer({
-  typeDefs,
-  resolvers,
-})
-
-apolloServer.start().then(() =>
-  app.use(
-    '/graphql',
-    optionalAuth,
-    expressMiddleware(apolloServer, {
-      context: async ({ req }) => {
-        return { auth: req.auth }
-      },
-    }),
-  ),
-)
 
 const app = express()
 app.use(cors())
@@ -32,10 +12,23 @@ app.use(bodyParser.json())
 
 postsRoutes(app)
 userRoutes(app)
-eventRoutes(app)
 
 app.get('/', (req, res) => {
   res.send('Hello World from Express!')
 })
 
-export { app }
+const server = createServer(app)
+
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  },
+})
+io.on('connection', (socket) => {
+  console.log('user connected:', socket.id)
+  socket.on('disconnect', () => {
+    console.log('user disconnected:', socket.id)
+  })
+})
+
+export { server as app }
